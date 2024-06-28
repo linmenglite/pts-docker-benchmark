225045b85af862d711ec250a7c8dd2b7fd7eda1c
linmenglite <1017345395@qq.com>
2024-06-28 19:17:12 +0800

    update

---
+++ build.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+PATH="$pwd"
+
+#下载PTS所需相关依赖
+#yum install -y php-cli php-gd php-xml
+
+#安装PTS
+git clone https://github.com/phoronix-test-suite/phoronix-test-suite.git
+if [ -d "phoronix-test-suite" ]; then  
+ 
+else  
+    exit 1 
+    echo "clone失败."  
+fi
+cd phoronix-test-suite
+sudo ./install-sh
+phoronix-test-suite user-config-set AnonymousUsageReporting=FALSE  
+phoronix-test-suite user-config-set DynamicRunCount=FALSE  
+phoronix-test-suite user-config-set UploadResults=FALSE  
+phoronix-test-suite user-config-set PromptForTestIdentifier=FALSE  
+phoronix-test-suite user-config-set PromptForTestDescription=FALSE  
+phoronix-test-suite user-config-set PromptSaveName=FALSE  
+phoronix-test-suite user-config-set Configured=TRUE
+cd ..
+
+#下载测试
+phoronix-test-suite install-dependencies pts/fio-2.1.0 pts/compress-7zip-1.10.0
+phoronix-test-suite install pts/fio-2.1.0 pts/compress-7zip-1.10.0
+
+#安装本地测试
+mv -r custom-cpu-disk-benchmark ~/.phoronix-test-suite/test-suites/local
+mv -r customized-openssl-3.1.0 ~/.phoronix-test-suite/test-profiles/local
+mv -r customized-sysbench-1.0.0 ~/.phoronix-test-suite/test-profiles/local
+
+phoronix-test-suite install-dependencies customized-openssl-3.1.0 customized-sysbench-1.0.0
+phoronix-test-suite install customized-openssl-3.1.0 customized-sysbench-1.0.0
+
+
+
+
+
