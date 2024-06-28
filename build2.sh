9ceb0b7cb255097dadbc1a0b64432c06976166ad
linmenglite <140476514+linmenglite@users.noreply.github.com>
2024-06-28 21:50:58 +0800

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sleep 5
+
 mv  custom-cpu-disk-benchmark ~/.phoronix-test-suite/test-suites/local
 mv  customized-openssl-3.1.0 ~/.phoronix-test-suite/test-profiles/local
 mv  customized-sysbench-1.0.0 ~/.phoronix-test-suite/test-profiles/local
