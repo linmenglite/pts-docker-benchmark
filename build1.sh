7fededa5061380061ccf5dd9f5754bec77835752
linmenglite <140476514+linmenglite@users.noreply.github.com>
2024-06-28 23:07:05 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -30,9 +30,9 @@ phoronix-test-suite install pts/fio-2.1.0 pts/compress-7zip-1.10.0
 
 phoronix-test-suite list-available-test
 
-mv  custom-cpu-disk-benchmark ~/.phoronix-test-suite/test-suites/local
-mv  customized-openssl-3.1.0 ~/.phoronix-test-suite/test-profiles/local
-mv  customized-sysbench-1.0.0 ~/.phoronix-test-suite/test-profiles/local
+sudo mv custom-cpu-disk-benchmark ~/.phoronix-test-suite/test-suites/local
+sudo mv customized-openssl-3.1.0 ~/.phoronix-test-suite/test-profiles/local
+sudo mv customized-sysbench-1.0.0 ~/.phoronix-test-suite/test-profiles/local
 
 phoronix-test-suite install-dependencies customized-openssl-3.1.0 customized-sysbench-1.0.0
 phoronix-test-suite install customized-openssl-3.1.0 customized-sysbench-1.0.0
