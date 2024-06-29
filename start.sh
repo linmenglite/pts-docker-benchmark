165901468e5fd49ff05b317a4e45fc9b6ada989a
linmenglite <1017345395@qq.com>
2024-06-29 19:51:37 +0800

    update

--- start.sh
+++ start.sh
@@ -11,21 +11,21 @@ fi
 
 chmod a+x performance_counter_920.sh
 
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/compress-7zip" $pwd 
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Customized_Sysbench_CPU_multi_core" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Customized_Sysbench_CPU_single_core" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_4KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_32KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_256KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_sequential_read_4MB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_sequential_write_4MB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_4KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_32KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_256KB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_read_4MB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_write_4MB" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Multi_core" $pwd
-sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Single_core" $pwd
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/compress-7zip" "$PWD" 
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Customized_Sysbench_CPU_multi_core" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Customized_Sysbench_CPU_single_core" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_4KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_32KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_random_write_256KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_sequential_read_4MB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/disk_sequential_write_4MB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_4KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_32KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_randwrite_256KB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_read_4MB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/fio_write_4MB" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Multi_core" "$PWD"
+sudo ./performance_counter_920.sh "phoronix-test-suite batch-run local/custom-benckmark/Single_core" "$PWD"
 
 
 
